4302c73a7af49d6f2404ab343a612c44b4ba6fca
zksytmkn <zksytmkn4869@gmail.com>
2022-11-05 23:50:32 +0900

    Initialized application.rb

--- config/application.rb
+++ config/application.rb
@@ -11,6 +11,19 @@ module App
     # Initialize configuration defaults for originally generated Rails version.
     config.load_defaults 7.0
 
+    # Railsアプリデフォルトのタイムゾーン(deafult 'UTC')
+    config.time_zone = ENV["TZ"]
+
+    # データベースの読み書きに使用するタイムゾーン(:local | :utc(default))
+    config.active_record.default_timezone = :utc
+
+    # i18nで使われるデフォルトのロケールファイルの指定(default :en)
+    config.i18n.default_locale = :ja
+
+    # $LOAD_PATHにautoload pathを追加しない(Zeitwerk有効時false推奨)
+    config.add_autoload_paths_to_load_path = false
+
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
