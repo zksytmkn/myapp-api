8e99c6598cdbd2ff7d7b16c9d94b262beee83310
zksytmkn <zksytmkn4869@gmail.com>
2022-11-07 15:12:53 +0900

    Setup Test

--- Gemfile
+++ Gemfile
@@ -47,3 +47,5 @@ group :development do
   # gem "spring"
 end
 
+group :test do
+end
--- test/test_helper.rb
+++ test/test_helper.rb
@@ -3,6 +3,11 @@ require_relative "../config/environment"
 require "rails/test_help"
 
 class ActiveSupport::TestCase
+
+  parallelize_setup do |worker|
+    load "#{Rails.root}/db/seeds.rb"
+  end
+
   # Run tests in parallel with specified workers
   parallelize(workers: :number_of_processors)
 
