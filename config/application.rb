5d9f139aba0cc2a79b99b048e0e5a45de7cad861
zksytmkn <zksytmkn4869@gmail.com>
2022-11-12 14:05:26 +0900

    Setting Nginx

--- config/application.rb
+++ config/application.rb
@@ -16,6 +16,17 @@ module App
     config.i18n.default_locale = :ja
     config.add_autoload_paths_to_load_path = false
 
+    config.generators do |g|
+      g.test_framework :rspec,
+        fixtures: true,
+        model_specs: true,
+        request_specs: true,
+        view_specs: false,
+        helper_specs: false,
+        routing_specs: false,
+        controller_specs: false
+    end
+
     # Configuration for the application, engines, and railties goes here.
     #
     # These settings can be overridden in specific environments using the files
