11386cefd6eafc9e5937ae84f00bd44ee20977f3
zksytmkn <zksytmkn4869@gmail.com>
2022-11-28 14:23:24 +0900

    Add auth token controller test

--- config/application.rb
+++ config/application.rb
@@ -35,6 +35,9 @@ module App
     # config.time_zone = "Central Time (US & Canada)"
     # config.eager_load_paths << Rails.root.join("extras")
 
+    # Cookieを処理するmeddlewareを追加
+    config.middleware.use ActionDispatch::Cookies
+
     # Only loads a smaller set of middleware suitable for API only apps.
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
