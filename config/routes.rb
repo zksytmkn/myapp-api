78037ca52c5d3d144caabc1060c5413aeb9107bb
zksytmkn <zksytmkn4869@gmail.com>
2022-11-28 15:00:38 +0900

    Add projects controller and authenticate user test

--- config/routes.rb
+++ config/routes.rb
@@ -1,10 +1,14 @@
 Rails.application.routes.draw do
   namespace :api do
     namespace :v1 do
+      # auth_token
       resources :auth_token, only:[:create] do
         post :refresh, on: :collection
         delete :destroy, on: :collection
       end
+
+      # projects
+      resource :projects, only:[:index]
     end
   end
 end
