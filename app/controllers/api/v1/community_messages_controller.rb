996998c0a50ccc5bce7ee8571b2c9d9c43a32789
zksytmkn <zksytmkn4869@gmail.com>
2023-04-27 23:22:50 +0900

    Refactor code for improved readability and maintainability

--- app/controllers/api/v1/community_messages_controller.rb
+++ app/controllers/api/v1/community_messages_controller.rb
@@ -1,5 +1,4 @@
 class Api::V1::CommunityMessagesController < ApplicationController
-  before_action :set_community_message, only: %i[destroy]
 
   def show
     render json: CommunityMessage.where(community_id: params[:id]), include: [:user]
@@ -9,15 +8,7 @@ class Api::V1::CommunityMessagesController < ApplicationController
     CommunityMessage.create!(community_message_params)
   end
 
-  def destroy
-    @community_message.destroy!
-  end
-
   private
-
-  def set_community_message
-    @community_message = CommunityMessage.find(params[:id])
-  end
 
   def community_message_params
     params.permit(:communityMessage_content, :community_id, :user_id)
