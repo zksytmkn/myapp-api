5defcf1ea5a5a23b8359fa4cc14ee9c3df2aa02c
zksytmkn <zksytmkn4869@gmail.com>
2023-06-19 09:40:40 +0900

    test: CartモデルとCartコントローラのテスト追加

--- spec/factories/carts.rb
+++ spec/factories/carts.rb
@@ -1,5 +1,7 @@
 FactoryBot.define do
   factory :cart do
-    
+    user
+    product
+    quantity { 1 }
   end
 end
--- spec/models/cart_spec.rb
+++ spec/models/cart_spec.rb
@@ -1,5 +1,12 @@
 require 'rails_helper'
 
 RSpec.describe Cart, type: :model do
-  pending "add some examples to (or delete) #{__FILE__}"
+  it { should belong_to(:user) }
+  it { should belong_to(:product) }
+
+  it { should validate_presence_of(:user_id) }
+  it { should validate_presence_of(:product_id) }
+  it { should validate_presence_of(:quantity) }
+
+  it { should validate_uniqueness_of(:user_id).scoped_to(:product_id) }
 end
