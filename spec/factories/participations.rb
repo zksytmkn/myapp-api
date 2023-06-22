3f449331d6e1634babb713d247078222903393c8
zksytmkn <zksytmkn4869@gmail.com>
2023-06-23 01:12:41 +0900

    test: ParticipationコントローラとParticipationモデルのテスト追加

--- spec/factories/participations.rb
+++ spec/factories/participations.rb
@@ -1,5 +1,6 @@
 FactoryBot.define do
   factory :participation do
-    
+    user
+    community
   end
 end
--- spec/requests/api/v1/participations_spec.rb
+++ spec/requests/api/v1/participations_spec.rb
@@ -1,7 +1,57 @@
 require 'rails_helper'
 
-RSpec.describe "Api::V1::Participations", type: :request do
-  describe "GET /index" do
-    pending "add some examples (or delete) #{__FILE__}"
+RSpec.describe 'Api::V1::Participations', type: :request do
+  let(:user) { create(:user) }
+  let(:community) { create(:community) }
+
+  before do
+    allow_any_instance_of(Api::V1::ParticipationsController).to receive(:current_user).and_return(user)
+  end
+
+  describe 'GET /index' do
+    before do
+      create(:participation, user: user, community: community)
+      get '/api/v1/participations'
+    end
+
+    it 'returns a successful response' do
+      expect(response).to have_http_status(:success)
+    end
+  end
+
+  describe 'POST /create' do
+    let(:valid_attributes) { { participation: { community_id: community.id } } }
+
+    context 'with valid attributes' do
+      before do
+        post '/api/v1/participations', params: valid_attributes
+      end
+
+      it 'creates a new participation' do
+        expect(response).to have_http_status(:created)
+      end
+    end
+
+    context 'with invalid attributes' do
+      before do
+        post '/api/v1/participations', params: { participation: { community_id: nil } }
+      end
+
+      it 'does not create a new participation' do
+        expect(response).to have_http_status(:unprocessable_entity)
+      end
+    end
+  end
+
+  describe 'DELETE /destroy' do
+    let!(:participation) { create(:participation, user: user, community: community) }
+
+    before do
+      delete "/api/v1/participations/#{participation.community_id}"
+    end
+
+    it 'deletes the participation' do
+      expect(response).to have_http_status(:no_content)
+    end
   end
 end
