a9404165cb7b9ce025750e7d07d6412d5c52e5a0
zksytmkn <zksytmkn4869@gmail.com>
2023-07-15 12:50:25 +0900

    test: postUnfavoriteコントローラのテスト修正

--- spec/requests/api/v1/post_unfavorites_spec.rb
+++ spec/requests/api/v1/post_unfavorites_spec.rb
@@ -2,8 +2,9 @@ require 'rails_helper'
 
 RSpec.describe 'Api::V1::PostUnfavorites', type: :request do
   let(:user) { create(:user) }
-  let(:post) { create(:post, user: user) }
-  let!(:post_unfavorite) { create(:post_unfavorite, user: user, post: post) }
+  let!(:post_object) { create(:post, user: user) }
+  let!(:post_unfavorite) { create(:post_unfavorite, user: user, post: post_object) }
+  let(:headers) { { 'X-Requested-With': 'XMLHttpRequest' } }
 
   before do
     allow_any_instance_of(Api::V1::PostUnfavoritesController).to receive(:current_user).and_return(user)
@@ -11,7 +12,7 @@ RSpec.describe 'Api::V1::PostUnfavorites', type: :request do
 
   describe 'GET /index' do
     before do
-      get "/api/v1/post_unfavorites"
+      get "/api/v1/post_unfavorites", headers: headers
     end
 
     it 'returns the list of unfavorites' do
@@ -21,34 +22,39 @@ RSpec.describe 'Api::V1::PostUnfavorites', type: :request do
   end
 
   describe 'POST /create' do
+    let!(:post_unfavorite) { nil }
+    let(:valid_attributes) { { post_id: post_object.id } }
+    let(:invalid_attributes) { { post_id: nil } }
+
     context 'with valid parameters' do
       before do
-        post "/api/v1/post_unfavorites", params: { post_unfavorite: { post_id: post.id } }
+        post "/api/v1/post_unfavorites", params: { post_unfavorite: valid_attributes }, headers: headers
       end
 
       it 'creates a new unfavorite' do
         expect(response).to have_http_status(:created)
-        expect(PostUnfavorite.count).to eq(2)
+        expect(PostUnfavorite.count).to eq(1)
       end
     end
 
     context 'with invalid parameters' do
       before do
-        post "/api/v1/post_unfavorites", params: { post_unfavorite: { post_id: nil } }
+        post "/api/v1/post_unfavorites", params: { post_unfavorite: invalid_attributes }, headers: headers
       end
 
       it 'does not create a new unfavorite' do
         expect(response).to have_http_status(:unprocessable_entity)
-        expect(PostUnfavorite.count).to eq(1)
+        expect(PostUnfavorite.count).to eq(0)
       end
     end
   end
 
   describe 'DELETE /destroy' do
     before do
-      delete "/api/v1/post_unfavorites/#{post_unfavorite.id}"
+      post_favorite = create(:post_favorite, user: user, post: post_object)
+      delete "/api/v1/post_unfavorites/#{post_object.id}/user", headers: headers
     end
-
+  
     it 'deletes the unfavorite' do
       expect(response).to have_http_status(:no_content)
       expect(PostUnfavorite.count).to eq(0)
