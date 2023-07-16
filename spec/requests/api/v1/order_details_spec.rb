1a3f49f86b6105b5f77ffada6592b3f458554b82
zksytmkn <zksytmkn4869@gmail.com>
2023-07-16 11:37:50 +0900

    test: orderDetailコントローラのテスト修正

--- spec/requests/api/v1/order_details_spec.rb
+++ spec/requests/api/v1/order_details_spec.rb
@@ -2,18 +2,18 @@ require 'rails_helper'
 
 RSpec.describe "Api::V1::OrderDetails", type: :request do
   let!(:order_detail) { create(:order_detail) }
+  let(:valid_params) { { order_detail: { status: 'shipped' } } } # Moved this line
+  let(:headers) { { 'X-Requested-With': 'XMLHttpRequest' } }
 
   describe 'PATCH #update' do
     context 'with valid parameters' do
-      let(:valid_params) { { order_detail: { status: 'shipped' } } }
-
       it 'updates the order detail' do
-        patch "/api/v1/order_details/#{order_detail.id}", params: valid_params
+        patch "/api/v1/order_details/#{order_detail.id}", params: valid_params, headers: headers
         expect(order_detail.reload.status).to eq('shipped')
       end
 
       it 'returns a successful response' do
-        patch "/api/v1/order_details/#{order_detail.id}", params: valid_params
+        patch "/api/v1/order_details/#{order_detail.id}", params: valid_params, headers: headers
         expect(response).to be_successful
       end
     end
@@ -22,19 +22,19 @@ RSpec.describe "Api::V1::OrderDetails", type: :request do
       let(:invalid_params) { { order_detail: { status: nil } } }
 
       it 'does not update the order detail' do
-        patch "/api/v1/order_details/#{order_detail.id}", params: invalid_params
+        patch "/api/v1/order_details/#{order_detail.id}", params: invalid_params, headers: headers
         expect(order_detail.reload.status).to_not be_nil
       end
 
       it 'returns an error' do
-        patch "/api/v1/order_details/#{order_detail.id}", params: invalid_params
+        patch "/api/v1/order_details/#{order_detail.id}", params: invalid_params, headers: headers
         expect(response.body).to include('error')
       end
     end
 
     context 'with non-existent order detail id' do
       it 'returns a not found error' do
-        patch "/api/v1/order_details/9999999", params: valid_params
+        patch "/api/v1/order_details/9999999", params: valid_params, headers: headers
         expect(response.body).to include('注文詳細が見つかりません')
       end
     end
