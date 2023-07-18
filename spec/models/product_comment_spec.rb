e7bab6cacdb48a22511dc55349a96348d9af2c0c
zksytmkn <zksytmkn4869@gmail.com>
2023-07-18 23:38:00 +0900

    test: productCommentモデルのテスト修正

--- spec/models/product_comment_spec.rb
+++ spec/models/product_comment_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe ProductComment, type: :model do
   describe 'validations' do
+    subject { create(:product_comment) }
+
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:product_id) }
-    it { should validate_length_of(:content).is_at_most(200).allow_blank }
+    it { should validate_length_of(:content).is_at_most(200) }
   end
 
   describe 'associations' do
