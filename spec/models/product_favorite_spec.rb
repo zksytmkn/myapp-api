ad0413274043d6c1889ecb64f35c203045d7efaf
zksytmkn <zksytmkn4869@gmail.com>
2023-07-18 23:32:14 +0900

    test: productFavoriteモデルのテスト修正

--- spec/models/product_favorite_spec.rb
+++ spec/models/product_favorite_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe ProductFavorite, type: :model do
   describe 'validations' do
+    subject { create(:product_favorite) }
+
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:product_id) }
     it { should validate_uniqueness_of(:user_id).scoped_to(:product_id) }
