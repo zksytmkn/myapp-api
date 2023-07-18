ac8e1d10b7241c820d26b37b182859aa7c4ace55
zksytmkn <zksytmkn4869@gmail.com>
2023-07-18 23:53:55 +0900

    test: postUnfavoriteモデルのテスト修正

--- spec/models/post_unfavorite_spec.rb
+++ spec/models/post_unfavorite_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe PostUnfavorite, type: :model do
   describe 'validations' do
+    subject { create(:post_unfavorite) }
+
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:post_id) }
     it { should validate_uniqueness_of(:user_id).scoped_to(:post_id) }
