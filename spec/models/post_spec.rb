bae9c6450ad63a1980f667921203c01c4bda6c93
zksytmkn <zksytmkn4869@gmail.com>
2023-07-19 00:05:56 +0900

    test: postモデルのテスト修正

--- spec/models/post_spec.rb
+++ spec/models/post_spec.rb
@@ -2,9 +2,11 @@ require 'rails_helper'
 
 RSpec.describe Post, type: :model do
   describe 'validations' do
+    subject { create(:post) }
+
     it { should validate_presence_of(:user_id) }
-    it { should validate_length_of(:title).is_at_most(30).allow_blank }
-    it { should validate_length_of(:body).is_at_most(400).allow_blank }
+    it { should validate_length_of(:title).is_at_most(30) }
+    it { should validate_length_of(:body).is_at_most(400) }
   end
 
   describe 'associations' do
