e14ff1b5092ac350a85685da61194785ecdf72d0
zksytmkn <zksytmkn4869@gmail.com>
2023-07-19 00:08:49 +0900

    test: participationモデルのテスト修正

--- spec/models/participation_spec.rb
+++ spec/models/participation_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Participation, type: :model do
   describe 'validations' do
+    subject { create(:participation) }
+
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:community_id) }
     it { should validate_uniqueness_of(:user_id).scoped_to(:community_id) }
