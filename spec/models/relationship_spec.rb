25a1ba37d0922ea742c17a7f11ec079deda1f601
zksytmkn <zksytmkn4869@gmail.com>
2023-07-18 23:22:34 +0900

    test: relationshipモデルのテスト修正

--- spec/models/relationship_spec.rb
+++ spec/models/relationship_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe Relationship, type: :model do
   describe 'validations' do
+    subject { create(:relationship) }
+
     it { should validate_presence_of(:following_id) }
     it { should validate_presence_of(:followed_id) }
     it { should validate_uniqueness_of(:following_id).scoped_to(:followed_id) }
