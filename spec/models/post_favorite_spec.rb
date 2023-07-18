218ca416b3f8de59e8495f2d2d8f92833de20488
zksytmkn <zksytmkn4869@gmail.com>
2023-07-18 23:57:55 +0900

    test: postFavoriteモデルのテスト修正

--- spec/models/post_favorite_spec.rb
+++ spec/models/post_favorite_spec.rb
@@ -2,6 +2,8 @@ require 'rails_helper'
 
 RSpec.describe PostFavorite, type: :model do
   describe 'validations' do
+    subject { create(:post_favorite) }
+
     it { should validate_presence_of(:user_id) }
     it { should validate_presence_of(:post_id) }
     it { should validate_uniqueness_of(:user_id).scoped_to(:post_id) }
