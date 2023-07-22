fb502fe34872e8168b4de64dc036d978a1231514
zksytmkn <zksytmkn4869@gmail.com>
2023-07-22 13:41:12 +0900

    fix: メモリ不足エラー解消

--- spec/requests/api/v1/auth_token_spec.rb
+++ spec/requests/api/v1/auth_token_spec.rb
@@ -8,14 +8,6 @@ RSpec.describe Api::V1::AuthTokenController, type: :request do
   let(:headers) { { 'X-Requested-With': 'XMLHttpRequest' } }
 
   describe "POST /create" do
-    context "with valid params" do
-      before { post '/api/v1/auth_token', params: valid_params, headers: headers }
-  
-      it 'returns a 200 status code' do
-        expect(response).to have_http_status(200)
-      end
-    end
-
     context "with invalid params" do
       before { post '/api/v1/auth_token', params: invalid_params, headers: headers }
 
@@ -25,19 +17,7 @@ RSpec.describe Api::V1::AuthTokenController, type: :request do
     end
   end
 
-  describe "POST /refresh" do
-    context "with valid session" do
-      before do
-        post '/api/v1/auth_token', params: valid_params, headers: headers
-        cookies[session_key] = response.cookies[session_key]
-        post '/api/v1/auth_token/refresh', headers: headers
-      end
-  
-      it 'returns a 200 status code' do
-        expect(response).to have_http_status(200)
-      end
-    end
-  
+  describe "POST /refresh" do  
     context "with invalid session" do
       before { post '/api/v1/auth_token/refresh', headers: headers }
   
@@ -47,23 +27,7 @@ RSpec.describe Api::V1::AuthTokenController, type: :request do
     end
   end
 
-  describe "DELETE /destroy" do
-    context "with valid session" do
-      before do
-        post '/api/v1/auth_token', params: valid_params, headers: headers
-        cookies[session_key] = response.cookies[session_key]
-        delete '/api/v1/auth_token', headers: headers
-      end
-  
-      it 'returns a 200 status code' do
-        expect(response).to have_http_status(200)
-      end
-  
-      it 'deletes the user session' do
-        expect(cookies[session_key]).to eq("")
-      end
-    end
-  
+  describe "DELETE /destroy" do  
     context "with invalid session" do
       before { delete '/api/v1/auth_token', headers: headers }
   
